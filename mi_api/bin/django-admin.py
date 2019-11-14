c780a4408225aaabffa734be1d23acea132c456f
Hugo <hugojquinterop@gmail.com>
2019-11-14 01:23:08 -0300

    env y Django
    Preparacion de entorno virtual e instalacion de Django

---
+++ mi_api/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/hugokan/Documents/IngenieriaTB2/Ingrenieria-django2/mi_api/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
